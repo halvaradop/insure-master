9e70c52ad5227f1713f7071d4233030583c87a51
halvaradop_ <hernanvid123@gmail.com>
2023-06-20 13:35:52 -0500

    init project

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./index.html', './src/**/*.{ts,tsx}'],
+  darkMode: 'class',
+  future: {
+    hoverOnlyWhenSupported: true
+  },
+  theme: {
+    extend: {
+      colors: {
+        blue: 'hsl(216, 30%, 68%)',
+        gray: 'hsl(0, 0%, 98%)',
+        violet: {
+          DEFAULT: 'hsl(256, 26%, 20%)',
+          100: 'hsl(273, 4%, 51%)',
+          200: 'hsl(270, 9%, 17%)'
+        }
+      },
+    },
+  },
+  plugins: [],
+}
+
