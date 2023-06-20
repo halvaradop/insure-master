010ab81601f10dd30967789589f2a359f0226e44
halvaradop_ <hernanvid123@gmail.com>
2023-06-20 18:05:11 -0500

    footer section with responsive design

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -5,11 +5,11 @@ import { ListItems } from "./ListItems"
 
 const Footer = () => {
     return (
-        <footer className="w-full bg-gray">
-            <section className="w-11/12 mx-auto">
-                <figure className="pt-24 pb-6 px-5 flex items-center flex-col gap-y-4 relative border-b border-blue">
+        <footer className="w-full bg-gray base:relative">
+            <section className="w-11/12 mx-auto base:w-10/12">
+                <figure className="pt-24 pb-6 px-5 flex items-center flex-col gap-y-4 relative border-b border-blue base:flex-row base:justify-between">
                     <img className="relative z-10" src={logo} alt="logo icon" />
-                    <figure className="flex items-center gap-x-4 relative z-10">
+                    <figure className="flex items-center gap-x-4 relative z-10 base:ml-auto">
                         {companiesFooter.map((path, key) => <img className="" key={key} src={path} />)}
                     </figure>
                     <picture>
@@ -17,7 +17,7 @@ const Footer = () => {
                         <img className="absolute top-0 left-0" src={bgFooter.bgFooterMobile} alt="background pattern mobile" />
                     </picture>
                 </figure>
-                <section className="mt-8 mb-14 grid gap-y-8">
+                <section className="mt-8 mb-14 grid gap-y-8 base:grid-cols-4">
                     <ListItems listItems={ourCompanyFooterList} />
                     <ListItems listItems={helpMeFooterList} />
                     <ListItems listItems={contactFooterList} />
